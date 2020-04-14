c9bdb7d1d6d77d69cbb78c6633ab006a92ad58a8
J.I. Neuteboom <jonathan.neuteboom@gmail.com>
2020-04-14 22:45:32 +0200

    npm-ify

--- services/IOService.js
+++ services/IOService.js
@@ -1,7 +1,7 @@
 (function (app) {
   app.service('IOService', function ($http, $q, Notification) {
-    var baseUrl = 'https://www.skcvolleybal.nl/tc-app/php/interface.php'
-    // var baseUrl = '/joomla-website/tc-app/php/interface.php';
+    // var baseUrl = 'https://www.skcvolleybal.nl/tc-app/php/interface.php'
+    var baseUrl = '/joomla-website/tc-app/php/interface.php';
 
     var executeAction = function (action, obj) {
       console.log(action);
