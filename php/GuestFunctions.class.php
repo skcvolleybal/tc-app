71cb06cb5b21547eb61eecf43f4f4feda6af0184
KM Schellens <k.schellens@gmail.com>
2023-05-24 17:09:37 +0200

    TC now checks with WordPress if a user is logged in

--- php/GuestFunctions.class.php
+++ php/GuestFunctions.class.php
@@ -7,10 +7,10 @@
       }
 
       public function Login($postObject){
-         $credentials = [
-            'username' => $postObject['username'],
-            'password' => $postObject['password']
-         ];
+         // $credentials = [
+         //    'username' => $postObject['username'],
+         //    'password' => $postObject['password']
+         // ];
 
          $result = wp_authenticate($postObject['username'], $postObject['password']);
 
@@ -26,34 +26,38 @@
          die();
 
 
-         $joomlaApp = JFactory::getApplication('site');
+         // $joomlaApp = JFactory::getApplication('site');
          
-         $db = JFactory::getDbo();
-         $query = $db->getQuery(true)
-            ->select('id, password')
-            ->from('#__users')
-            ->where('username=' . $db->quote($credentials['username']));
+         // $db = JFactory::getDbo();
+         // $query = $db->getQuery(true)
+         //    ->select('id, password')
+         //    ->from('#__users')
+         //    ->where('username=' . $db->quote($credentials['username']));
       
-         $db->setQuery($query);
-         $result = $db->loadObject();
-         if ($result) {
-            $match = JUserHelper::verifyPassword($credentials['password'], $result->password, $result->id);
-            if ($match === true) {
-               $joomlaApp->login($credentials);
-               $dataLists = $this->lists->GetLists();
-               return ['message' => "Opnieuw ingelogd", 'dataLists' => $dataLists];
-            }
-            else {      
-               throw new Exception('Fout wachtwoord, probeer het nog eens');
-            }
-         } else {
-            throw new Exception("Gebruiker '" . $credentials['username'] . "' bestaat niet");
-         }
+         // $db->setQuery($query);
+         // $result = $db->loadObject();
+         // if ($result) {
+         //    $match = JUserHelper::verifyPassword($credentials['password'], $result->password, $result->id);
+         //    if ($match === true) {
+         //       $joomlaApp->login($credentials);
+         //       $dataLists = $this->lists->GetLists();
+         //       return ['message' => "Opnieuw ingelogd", 'dataLists' => $dataLists];
+         //    }
+         //    else {      
+         //       throw new Exception('Fout wachtwoord, probeer het nog eens');
+         //    }
+         // } else {
+         //    throw new Exception("Gebruiker '" . $credentials['username'] . "' bestaat niet");
+         // }
       }
 
       public function CheckIfuserIsLoggedIn(){
-         $user = JFactory::getUser();
+         $wpuser = wp_get_current_user();
+         // $user = JFactory::getUser(); Joomla code
          $dataLists = $this->lists->GetLists();
-         return ['userIsloggedIn' => $user->id != 0, 'dataLists' => $dataLists];
+         
+         // return ['userIsloggedIn' => $user->id != 0, 'dataLists' => $dataLists]; Joomla code
+
+         return ['userIsloggedIn' => $wpuser->ID != 0, 'dataLists' => $dataLists];
       }
    }
