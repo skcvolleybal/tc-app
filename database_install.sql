9fb394c13b90216ea1b3b019e1f513c6ef3c7ec1
Koen Schellens <k.schellens@gmail.com>
2024-09-15 14:48:05 +0200

    working?

--- database_install.sql
+++ database_install.sql
@@ -1,8 +1,8 @@
 
 ```
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = '+00:00';
 
 
 CREATE TABLE `tcapp_players` (
